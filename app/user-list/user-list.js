12c643d3d8bdb483f4ad614ca7868cf2c0f5b740
Irene Fung <irene.ifung@gmail.com>
2015-06-14 22:49:45 -0700

    Namespaced the User List Controller and removed addUsers from a directive into controller

--- app/user-list/user-list.js
+++ app/user-list/user-list.js
@@ -1,44 +1,40 @@
 angular.module('userMgmtApp.user-list', ['ngRoute'])
-    .config(['$routeProvider', function ($routeProvider) {
+    .controller('UserListCtrl', [ '$scope', 'uuid4', 'userFactory', 'userService', function ($scope, uuid4, userFactory, userService) {
         'use strict';
+        
+        var ULC = this;
+        
+        ULC.itemsByPage = 5;
+        
+        //ULC.userIds = userFactory.get('userIds');
+        ULC.users = userFactory.getAll();
+        ULC.displayedUsers = [].concat(ULC.users);
+        
+        // Adds a new user to local storage
+        ULC.addUser = function () {
+            var newId = uuid4.generate(),
+                newUser = { id: newId,
+                           username: ULC.username, password: ULC.password,
+                           created: (new Date()), updated: (new Date()) };
 
-        $routeProvider.when('/users', {
-            templateUrl: 'user-list/user-list.html',
-            controller: 'UserListCtrl'
-        });
-    }])
-    .controller('UserListCtrl', [ '$scope', 'uuid4', 'usersFactory', function ($scope, uuid4, usersFactory) {
-        'use strict';
-        
-        $scope.itemsByPage = 5;
-        
-        $scope.userIds = usersFactory.get('userIds') || [];
-        $scope.users = usersFactory.getAll() || [];
+            // Save to local storage
+            userFactory.set(newId, newUser);
+
+            // Add new user ID to the array
+            userFactory.addId(newId);
+
+            // Get all users into scope
+            ULC.users = userFactory.getAll();
             
-        // Watch the array of userIDs, updates the list of users in the scope
-        $scope.$watchCollection('userIds', function () {
-            usersFactory.set('userIds', $scope.userIds);
-            $scope.users = usersFactory.getAll();
-        });
-        
-        $scope.displayedUsers = [].concat($scope.users);
+            // Reset the form
+            ULC.username = '';
+            ULC.password = '';
+            $scope.userForm.$setPristine();
+        };
         
         // Checks to see if username is unique
-        $scope.uniqueUsername = function (value, index) {
-            var idx;
-
-            // If editing an user, the original 
-            if (index > -1 && (!value || value === $scope.users[index].username)) {
-                return true;
-            }
-
-            for (idx = 0; idx < $scope.users.length; idx = idx + 1) {
-                if ($scope.users[idx].username === value) {
-                    return false;
-                }
-            }
-
-            return true;
+        ULC.uniqueUsername = function (value, index) {
+            return userService.uniqueUsername(value, index);
         };
     }])
     .directive('stRatio', function () {
@@ -50,36 +46,5 @@ angular.module('userMgmtApp.user-list', ['ngRoute'])
 
                 element.css('width', ratio + '%');
             }
-        };
-    })
-    .directive('myAddUser', function (uuid4, usersFactory) {
-        'use strict';
-
-        return {
-            restrict: 'E',
-            controller: function ($scope) {
-                
-                // Adds a new user to local storage
-                $scope.addUser = function () {
-                    var newId = uuid4.generate(),
-                        newUser = { id: newId,
-                                   username: $scope.username,
-                                   password: $scope.password,
-                                   created: (new Date()),
-                                   updated: (new Date()) };
-                    
-                    // Save to local storage
-                    usersFactory.set(newId, newUser);
-
-                    // Add new user ID to the array
-                    $scope.userIds.push(newId);
-                    
-                    // Reset the form
-                    $scope.username = '';
-                    $scope.password = '';
-                    $scope.userForm.$setPristine();
-                };
-            },
-            templateUrl: 'user-list/my-add-user.html'
         };
     });
